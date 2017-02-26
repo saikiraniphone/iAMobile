f0498e66c44f7764bf0d081751d03c2b80b32f60
Ramesh <ramesh.m0408@gmail.com>
2017-02-26 22:53:51 +0530

    added flow for login

--- IAMobile/IAMobile/LoginViewController.swift
+++ IAMobile/IAMobile/LoginViewController.swift
@@ -20,10 +20,17 @@ class LoginViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
