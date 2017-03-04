bf0d6aadd0f3a4972e2b1cda168d3dac270f3cb6
sai.iphone <saikiran.iphone@gmail.com>
2017-03-04 22:19:12 +0530

    Singleton
    Created sample singleton class.

--- IAMobile/IAMobile/AppDelegate.swift
+++ IAMobile/IAMobile/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		
 		// Override point for customization after application launch.
 		SideMenuController.preferences.drawing.menuButtonImage = UIImage(named: "act_menu")
 
@@ -23,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		SideMenuController.preferences.drawing.centerPanelShadow = true
 		SideMenuController.preferences.animating.statusBarBehaviour = .fadeAnimation
 
+		Utilities.sharedInstance.testMethod()
+		
 		return true
 	}
 
---
+++ IAMobile/IAMobile/SideMenu/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  Utilities.swift
+//  IAMobile
+//
+//  Created by sai on 03/03/17.
+//  Copyright © 2017 Sai. All rights reserved.
+//
+
+import Foundation
+class Utilities {
+	
+	static let sharedInstance = Utilities()
+	
+	func testMethod()  {
+		print("test method")
+	}
+	
+}
